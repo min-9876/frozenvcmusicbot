f36fa77fdc4888182ab123d4bf0cbef356584e99
min-9876 <musicmin2029@gmail.com>
2026-05-26 22:34:33 +0630

    Update startup_hooks.py

--- FrozenMusic/telegram_client/startup_hooks.py
+++ FrozenMusic/telegram_client/startup_hooks.py
@@ -4,7 +4,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 async def precheck_channels(client):
-    targets = ["@kustbots", "@kustbotschat"]
+    targets = ["@myanmar_music_Bot2027", "@myanmarbot_music"]
     for chan in targets:
         try:
             await client.join_chat(chan)
